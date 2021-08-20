1c2d763aabbdab37f46e48df6a81ff886f6609ce
Guilherme <guilherme@nfe.io>
2021-08-20 18:40:10 -0300

    fix class name

--- src/proposal/proposal.controller.spec.ts
+++ src/proposal/proposal.controller.spec.ts
@@ -1,17 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { PropostaController } from './proposal.controller';
-import { PropostaService } from './proposal.service';
+import { ProposalController } from './proposal.controller';
+import { ProposalService } from './proposal.service';
 
 describe('PropostaController', () => {
-  let controller: PropostaController;
+  let controller: ProposalController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [PropostaController],
-      providers: [PropostaService],
+      controllers: [ProposalController],
+      providers: [ProposalService],
     }).compile();
 
-    controller = module.get<PropostaController>(PropostaController);
+    controller = module.get<ProposalController>(ProposalController);
   });
 
   it('should be defined', () => {
