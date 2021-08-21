a47bae0d2e20d1bbc059582117cc286c66be3263
Guilherme <guilherme@nfe.io>
2021-08-20 21:47:38 -0300

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  
+  const config = new DocumentBuilder()
+    .setTitle('Calculadora de proposta')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('calculadora')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
